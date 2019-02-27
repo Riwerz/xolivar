d0030541449161717787e55565f68ecd0c79fb9c
EgorPopruga <najibator228@gmail.com>
2019-02-27 22:00:27 +0300

    Added login app

---
+++ login/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+
+
+def log_in(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login/LoginForm.html', context={'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'login/RegistrationForm.html', context={'form': form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect('/')
